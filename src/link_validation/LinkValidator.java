76048f1cc32fee70b87882a8cc30ca618b396daf
Massy BOURENNANI <cm_bourennani@esi.dz>
2018-01-23 12:47:55 +0100

    changing contract for linkValidator

--- src/link_validation/LinkValidator.java
+++ src/link_validation/LinkValidator.java
@@ -1,10 +1,17 @@
 package link_validation;
 
+import com.sun.javafx.scene.control.ReadOnlyUnbackedObservableList;
+import org.apache.jena.ext.com.google.common.primitives.Doubles;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import rdf_data.RDFManager;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class LinkValidator {
 
@@ -12,32 +19,78 @@ public class LinkValidator {
     private double threshold;
     private Agregation agregationFunction;
     int depth;
+    List<Property> propertiesToConsider;
 
-    public LinkValidator(double threshold, int depth, Agregation agregationFunction){
+    public LinkValidator(double threshold, int depth, Agregation agregationFunction,
+                         List<Property> properties){
+
         this.threshold = threshold;
         this.depth = depth;
         this.agregationFunction  = agregationFunction;
+        this.propertiesToConsider = properties;
+    }
+
+    // a default constructor that uses the average for agregating
+    public LinkValidator(double threshold, int depth, List<Property> properties){
+
+        this.threshold = threshold;
+        this.depth = depth;
+        this.agregationFunction  = Agregation.AVG;
+        this.propertiesToConsider = properties;
     }
 
     public Map<String, String> validateLinks(Map<String, String> mappingToValidate,
-                                             Model rdf1, Model rdf2, List<Property> propertiesToConsider){
+                                             Model rdf1, Model rdf2){
 
-        //TODO implement
-        return null;
+        Map<String, String> validMappings = new HashMap<>();
+
+        for(String entity1URI: mappingToValidate.keySet()){
+            String entity2URI = mappingToValidate.get(entity1URI);
+
+            // getting the resources form the rdf files
+            Resource entity1 = rdf1.getResource(entity1URI);
+            Resource entity2 = rdf2.getResource(entity2URI);
+
+            // computing the contextual graphs for both
+            Model contextualGraph1 = RDFManager.getContextualGraph(entity1, depth,
+                                                propertiesToConsider, rdf1);
+
+            Model contextualGraph2 = RDFManager.getContextualGraph(entity2, depth,
+                                                propertiesToConsider, rdf2);
+
+            //getting the resources to the contextual graph
+            entity1 = contextualGraph1.getResource(entity1URI);
+            entity2 = contextualGraph2.getResource(entity2URI);
+
+            // see if it's valid
+            if(isValidLink(entity1, entity2)){
+                validMappings.put(entity1URI, entity2URI);
+            }
+        }
+        return validMappings;
     }
 
-    public Map<String, String> validateLinks(Map<String, String> mappingToValidate,
-                                             Model rdf1, Model rdf2, List<Property> propertiesToConsider,
-                                             Map<String, String> propertiesMapping){
-        //TODO implment
-        return null;
-    }
 
-    public boolean isValidLink(String entity1URI, String entityURI2, Model rdf1, Model rdf2){
+
+    public boolean isValidLink(RDFNode entity1, RDFNode entity2){
         //TODO implement
         return false;
     }
 
+    private double average(Set<Double> similarities){
+        //TODO return the average of the set
+        return 0;
+    }
+
+    private double min(Set<Double> similarities){
+        //TODO return the min of the set
+        return 0;
+    }
+
+    private double max(Set<Double> similarities){
+        //TODO return the max of the set
+        return 0;
+    }
 
 
 }

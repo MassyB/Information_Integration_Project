7a707cf437a2651d5c8c6a1d363be539e4b840bc
Massy BOURENNANI <cm_bourennani@esi.dz>
2018-01-26 17:25:00 +0100

    commenting RDFManager

--- src/rdf_data/RDFManager.java
+++ src/rdf_data/RDFManager.java
@@ -12,6 +12,20 @@ import java.util.Set;
 
 public class RDFManager {
 
+    /**
+     *  @param resource the rdf resource used to compute the Contextual graph
+     *  @param depth the maximal depth to reach during construction of the contextual graph
+     *  @param propertiesToConsider set containing URI of properties to explore during construction
+     *                              of contextual graph
+     *  @param originalGraph the graph containing the contextual graph
+     *
+     *  @return a Jena Model object containing the contextual graph of resource
+     *
+     *  this method computes the contextual graph of a node given a set of properties to consider
+     *  and a maximal depth to reach.
+     *
+     * */
+
     public static Model getContextualGraph(Resource resource, int depth,
                                            Set<Property> propertiesToConsider, Model originalGraph) {
 
@@ -25,7 +39,27 @@ public class RDFManager {
 
     }
 
-    public static Set<Statement> getContextualGraph(Model model, Resource resource, Set<RDFNode> visited,
+    /**
+     *  @param model the graph containing the contextual graph
+     *  @param resource the rdf resource used to compute the Contextual graph
+     *  @param visited
+     *  @param propertiesToConsider set containing URI of properties to explore during construction
+     *                              of contextual graph
+     *  @param reachedDepth the current depth
+     *  @param depth the maximal depth to reach during construction of the contextual graph
+     *  @param removeLastEntityFalg this flag is set when an entity needs to be removed for
+     *                              the contextual graph
+     *  @param supressStmtWithObject used to triger a flag and rid of the paths that don't end with a literal
+     *
+     *  @return a set of statements reachable from the resource
+     *
+     *  This function is a recursive one, each call returns a set of statements reachable from
+     *  the current resource given that the maximal depth is not reached and only properties of
+     *  propertiesTocConsider are used.
+     *
+     * */
+
+    private static Set<Statement> getContextualGraph(Model model, Resource resource, Set<RDFNode> visited,
                                                     Set<Property> propertiesToConsider, int reachedDepth, int depth,
                                                     boolean[] removeLastEntityFalg, boolean[] supressStmtWithObject) {
 
@@ -67,13 +101,14 @@ public class RDFManager {
                             depth, removeLastEntityFalg,
                             supressStmtWithObject);
                     if (removeLastEntityFalg[0]) {
-                        // inc the number of flaged entities
 
+                        // inc the number of flaged entities
                         nbFlagedResourceObject++;
                         removeLastEntityFalg[0] = false;
                     }
 
                     if (supressStmtWithObject[0]) {
+
                         contextualGraph.remove(stmt);
                         supressStmtWithObject[0] = false;
                     }
@@ -92,6 +127,14 @@ public class RDFManager {
 
         return contextualGraph;
     }
+
+    /**
+     *  @param filePath path to the rdf file containing the RDF graph
+     *
+     *  @return reads an rdf graph from a file and returns model object
+     *          as defined in Jena
+     *
+     * */
 
     public static Model getRdfGraphFromFile(String filePath) throws FileNotFoundException {
 

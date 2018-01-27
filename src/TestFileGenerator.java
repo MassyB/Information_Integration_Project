c5baaaae935914e7fd9ff825b85c73502bab5211
Massy BOURENNANI <cm_bourennani@esi.dz>
2018-01-27 13:42:32 +0100

    In testFileGenerator, save the ground truth

--- src/TestFileGenerator.java
+++ src/TestFileGenerator.java
@@ -15,7 +15,9 @@ public class TestFileGenerator {
     /** #param 1 is the filePath to the gold standard
      *  #param 2 is the ratio, if ratio == 0.6, 60% of the sameAs links will be false
      *
-     *  generate mappings that have false same as links and save them in a tsv file
+     *  generate mappings that have false same as links and save them in a tsv file.
+     *  In addition to that file a gold standard is also generated, it contains only the
+     *  valid links of the test file.
      * */
 
     public static void main(String[] args) throws IOException {
@@ -25,10 +27,18 @@ public class TestFileGenerator {
 
         String goldStandardFilePath = args[filePathArg];
         double ratio  = Double.valueOf(args[ratioArg]);
-
+        // getting the gold standard form the rdf file
         Map<String, String> goldStandard = Utils.getSameAsLinks(goldStandardFilePath);
+        // generating test data
         Map<String, String> testMapping = Utils.getTestMapping(goldStandard, ratio);
+        // computing the valid links of the test data
+        Map<String, String> testMappingValidLinks = Utils.getIntersection(goldStandard, testMapping);
+
+        // saving the testMapping
         Utils.writeMappingIntoTsv(testMapping, goldStandardFilePath+"_test.tsv");
+        // saving the valid entries of testMapping
+        Utils.writeMappingIntoTsv(testMappingValidLinks, goldStandardFilePath+"_test_validLinks.tsv");
+
 
 
     }
